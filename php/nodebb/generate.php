ada16824afa1106bdf55cb83e8bee9c413158d78
Fábio Machado de Oliveira <machado2@gmail.com>
2016-03-16 18:28:29 -0300

    support for nodebb

---
+++ php/nodebb/generate.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<html>
+
+<noscript>This page works without javascript, but that's not intentional</noscript>
+
+<div class="container">
+
+<?php
+
+require '../markov.php';
+
+$username = urlencode( $_GET["username"]);
+if (strlen($username) < 3)
+    die('invalid username');
+
+$input = '';
+for ($page = 1; $page <= 10; $page++) {
+    $url = "https://discourse.local.lubar.me/api/user/" . $username . "/posts?page=" . $page;
+    $posts = json_decode(file_get_contents($url))->{'posts'};
+    foreach ($posts as $post) {
+        $input = $input . "\n\n" . html_entity_decode($post->{'content'}, ENT_QUOTES, 'UTF-8');
+    }
+}
+$input = str_replace("</p>", "\n", $input);
+$input = str_replace("<p>", "\n", $input);
+
+$order = (int) $_GET["order"];
+if ($order < 1 || $order > 99)
+    $order = 4;
+
+$markov_table = generate_markov_table($input, $order);
+$markov = generate_markov_text(2500, $markov_table, $order);
+$markov = htmlspecialchars($markov);
+$markov = '<p>' . str_replace("\n\n", '</p><p>', $markov) . '</p>';
+
+echo $markov;
+
---
+++ php/nodebb/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
+<html>
+
+<noscript>This page works without javascript, but that's not intentional</noscript>
+
+<div class="container">
+
+<p>Source code is on <a href="https://github.com/machado2/thedailywtf-markov-generator">github</a></p>
+
+<form action="generate.php" method="get">
+<div class="form-horizontal">
+    <div class="form-group">
+        <label for="order" class="control-label col-xs-2">Order</label>
+        <div class="col-xs-10">
+            <input type="text" id="order" name="order" value="4" />
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="username" class="control-label col-xs-2">Username</label>
+        <div class="col-xs-10">
+            <input type="text" id="username" name="username" autofocus />
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-xs-offset-2 col-xs-10">
+            <button id="btnbuscar" class="btn btn-default">generate</button>
+        </div>
+    </div>
+</div>
+</form>
+
+</div>
+
+</html>
